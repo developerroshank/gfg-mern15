2f791817138667d14b7349df52919dce94164121
developerroshank <developerroshank@gmail.com>
2025-07-08 21:18:39 +0530

    update: assignment ans9

--- WE-4/assignment/ans9.js
+++ WE-4/assignment/ans9.js
@@ -2,7 +2,17 @@
 // [[1, 2], [3, 4], [5]]
 // output=>[1,2,3,4,5]
 
+// Solution 1
+
 let arr = [[1, 2], [3, 4], [5]];
-arr = arr.reduce((a, b) => a.concat(b))
+arr = arr.reduce((a, b) => a.concat(b));
 
 console.log(arr);
+
+
+// Solution 2
+
+let a = [[1, 2], [3, 4], [5]];
+a = a.flat(Infinity);
+
+console.log(a);
