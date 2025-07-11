2e7372be789b0cc2471241d2796b1284d7d0ec3a
developerroshank <developerroshank@gmail.com>
2025-07-11 19:23:47 +0530

    update: destructuring

--- WE-5/destructuring.js
+++ WE-5/destructuring.js
@@ -1,3 +1,29 @@
+/**
+ * Destructuring ==> ES6
+ */
+
+// Object == key value pair
+
+// const obj = {
+//     name: "roshan",
+//     age: 200,
+//     isStudent: true,
+//     address: {
+//         room: 11,
+//         city: "Noida",
+//         state: "Uttar Pradesh"
+//     },
+//     skills: ['a', 'b', 'c']
+// }
+
+// console.log(obj.age)
+
+
+// To upper case skills
+
+// const toSentenceCase = obj.skills.map(e => e.toUpperCase())
+// console.log(toSentenceCase)
+
 const data = [
     {
         name: 'harsh',
@@ -23,7 +49,36 @@ const data = [
         name: 'akash',
         age: 19
     },
-]
+];
 
-const p = data.map(e => e.name[0].toUpperCase() + e.name.slice(1))
-console.log(p)
+// ==> Access all name
+const n = data.map(e => e.name)
+console.log(n);
+
+// ===> Covert name first letter cpas to small || small to caps
+
+const p = data.map(e => e.name[0].toUpperCase() + e.name.slice(1));
+console.log(p);
+
+
+
+/**
+ * Object.keys, Object.values, Object.entries
+ */
+
+const obj = {
+    name: "roshan",
+    age: 200,
+    isStudent: true,
+    address: {
+        room: 11,
+        city: "Noida",
+        state: "Uttar Pradesh"
+    },
+    skills: ['a', 'b', 'c']
+}
+
+
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+console.log(Object.entries(obj));
