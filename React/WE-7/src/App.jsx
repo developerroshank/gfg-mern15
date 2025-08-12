80683b87dc98d6a42269a5bf2dc8e911a5805146
developerroshank <developerroshank@gmail.com>
2025-08-12 20:04:51 +0530

    update: Button

--- React/WE-7/src/App.jsx
+++ React/WE-7/src/App.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import { useState } from 'react'
-// import Button from './Button'
+import Button from './Button'
 
 
 const App = () => {
 
   // Increament & Decreament
 
-  const[count,setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
-  const incHan = () => {
-    if (count < 10) {
-      setCount(count=>count+1)
-    }
-  }
-  const decHan = () => {
-    if(count > 0) {
+  // const incHan = () => {
+  //   if (count < 10) {
+  //     setCount(count => count + 1)
+  //   }
+  // }
+  // const decHan = () => {
+  //   if (count > 0) {
 
-      setCount(count=>count-1)
-    }
-  }
+  //     setCount(count => count - 1)
+  //   }
+  // }
 
   // Toggle 
   // const[name,setName] = useState(false)
@@ -28,23 +28,44 @@ const App = () => {
   //   setName(!name)
   // }
 
-
-
-
+  const buttons = [
+    {
+      title: 'Login',
+      backgroundColor: 'red',
+    },
+    {
+      title: 'Sign Up',
+      backgroundColor: 'yellow',
+    },
+    {
+      title: 'Submit',
+      backgroundColor: 'blue',
+    },
+    {
+      title: 'Login',
+      backgroundColor: 'green',
+    },
+  ]
 
   return (
     <>
       {/* Increament & Decreament */}
-      <h1>{count}</h1>
+      {/* <h1>{count}</h1>
       <button onClick={incHan}>Increase</button>
-      <button onClick={decHan}>Decrease</button>
+      <button onClick={decHan}>Decrease</button> */}
 
       {/* Value Toggle */}
       {/* <h1>{name ? "abc" : "xyz"}</h1>
-      <button onClick={valueToggle}>Toggle</button> */}
+      // <button onClick={valueToggle}>Toggle</button> */}
 
 
-      {/* <Button title="login" backgroundColor="red"/> */}
+      {/* <Button title='Submit' backgroundColor='green'/> */}
+
+      {buttons.map(({ title, backgroundColor }) => {
+        <Button key={title} title={title} backgroundColor={backgroundColor} />
+      })}
+
+      <p>hello</p>
 
     </>
   )
--- React/WE-7/src/Button.jsx
+++ React/WE-7/src/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export const Button = (title, backgroundColor) => {
-
+const Button = ({ title, backgroundColor }) => {
   return (
     <>
       <button style={backgroundColor}>{title}</button>
     </>
   )
 }
+
+export default Button
