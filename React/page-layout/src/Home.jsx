29ec37a47a211f68d599235611f2755d14b704e3
developerroshank <developerroshank@gmail.com>
2025-08-12 20:06:47 +0530

    update: home.jsx

--- React/page-layout/src/Home.jsx
+++ React/page-layout/src/Home.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import './assets/css/Style.css'
-import Header from './Header'
-import Sidebar from './Sidebar'
-import Content from './Content'
-import Footer from './Footer'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import Content from './components/Content'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
+    <>
     <div className='parent'>
       <Header/>
       <Sidebar/>
       <Content/>
       <Footer/>
     </div>
+    </>
   )
 }
 
