08694827cf091e143a206fb71b4d677b20a08147
developerroshank <developerroshank@gmail.com>
2025-08-14 23:07:11 +0530

    Add all pages

---
+++ React/page-layout/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const About = () => {
+  return (
+    <div className='parent'>
+      <title>About</title>
+      <Header />
+    </div>
+  )
+}
+
+export default About
